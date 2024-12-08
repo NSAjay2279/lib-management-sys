73565b3bd1f769535544d0bdafa1a00a03fb61a5
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-08 17:27:48 +0000

    Sun, Dec 8, 2024, 10:57 PM +05:30

--- favorites.py
+++ favorites.py
@@ -1,15 +1,10 @@
 import csv
 
+from collections import Counter
+
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
-    counts = {}
-
-    for row in reader:
-        favorite = row["language"]
-        if favorite in counts:
-            counts[favorite] += 1
-        else:
-            counts[favorite] = 1
+    counts = Counter()
 
 for favorite in sorted(counts, key=counts.get, reverse=True):
     print(f"{favorite}: {counts[favorite]}")
