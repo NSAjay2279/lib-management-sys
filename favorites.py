5a511d497a408be161970b3133418562c74896fe
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-12-08 16:51:35 +0000

    Sun, Dec 8, 2024, 10:21 PM +05:30

--- favorites.py
+++ favorites.py
@@ -3,5 +3,5 @@ import csv
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        favorite = row[1]
+        favorite = row["language"]
         print(favorite)
